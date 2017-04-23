9df7f8f21d241ed51c7ebea282864afbdfa481e7
oswaldo.neto <oswaldo.neto@gmail.com>
2017-04-23 19:50:45 -0300

    ajustes na tela

--- simple/forms.py
+++ simple/forms.py
@@ -4,4 +4,4 @@ from django import forms
 
 
 class InputForm(forms.Form):
-    text = forms.CharField(widget=forms.Textarea)
+    text = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Write your own or paste some peace of text here...'}))
